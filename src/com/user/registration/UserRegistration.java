1909f3894557ba6efabe0f82f359e836b69e3a35
sambit420 <sambit2024@gmail.com>
2022-11-15 19:11:12 +0530

    Added : Use Case 3 Program - User need to enter a valid email id

--- src/com/user/registration/UserRegistration.java
+++ src/com/user/registration/UserRegistration.java
@@ -1,5 +1,7 @@
 //UC - 1 is to As a User need to enter a valid First Name. (First name starts with Cap and has minimum 3 characters)
 //UC - 2 is to As a User need to enter a valid Last Name. (Last name starts with Cap and has minimum 3 characters)
+//UC - 3 is to As a User need to enter a valid email. (E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co)
+//            and 2 optional (xyz & in) with precise @ and . positions)
 
 package com.user.registration;
 
@@ -15,21 +17,31 @@ public class UserRegistration {
         String firstName = scan.next();
         System.out.println("Enter the User's Last Name");
         String lastName = scan.next();
-        if (validName(firstName) && validName(lastName))
-        {
-            System.out.println("Given first name : " + firstName + " & last name : " + lastName + " is valid");
-        }
-        else
-        {
-            System.out.println("Given first name : " + firstName + " & last name : " + lastName + " is not valid");
-        }
+        System.out.println("Enter the User's email id");
+        String emailID = scan.next();
+        System.out.println("Given first name : " + firstName + " is valid : " + validName(firstName));
+        System.out.println("Given last name : " + lastName + " is valid : " + validName(lastName));
+        System.out.println("Given email id : " + emailID + " is valid : " + validEmail(emailID));
     }
 
     // Method to check username
     public static boolean validName(String name) {
         boolean flag = false;
-        String regex="[A-Z][a-z]{3,}";      // {3,} means above 3 char firstname
-        Pattern pattern=Pattern.compile(regex);
+        String regex = "^[A-Z][a-z]{3,}$";      // {3,} means above 3 char name
+        Pattern pattern = Pattern.compile(regex);
+        for(int i = 0; i < name.length(); i++)
+        {
+            Matcher matcher=pattern.matcher(name);
+            flag = matcher.matches();
+        }
+        return flag;
+    }
+
+    // Method to check email id
+    public static boolean validEmail(String name) {
+        boolean flag = false;
+        String regex = "^[a-z0-9+_.-]+@+[a-z]+.+[a-z]$";
+        Pattern pattern = Pattern.compile(regex);
         for(int i = 0; i < name.length(); i++)
         {
             Matcher matcher=pattern.matcher(name);
