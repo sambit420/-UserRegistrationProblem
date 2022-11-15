72def094889d22fc7c11e642f2b5cce525e409af
sambit420 <sambit2024@gmail.com>
2022-11-15 19:15:13 +0530

    Added : Use Case 4 Program - User need to enter a valid mobile no

--- src/com/user/registration/UserRegistration.java
+++ src/com/user/registration/UserRegistration.java
@@ -2,6 +2,8 @@
 //UC - 2 is to As a User need to enter a valid Last Name. (Last name starts with Cap and has minimum 3 characters)
 //UC - 3 is to As a User need to enter a valid email. (E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co)
 //            and 2 optional (xyz & in) with precise @ and . positions)
+//UC - 4 is to As a User need to follow pre-defined Mobile Format. (Mobile Format - E.g. 91 9919819801 -
+//            Country code follow by space and 10-digit number)
 
 package com.user.registration;
 
@@ -11,17 +13,20 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the User's First Name");
-        String firstName = scan.next();
+        String firstName = scan.nextLine();
         System.out.println("Enter the User's Last Name");
-        String lastName = scan.next();
+        String lastName = scan.nextLine();
         System.out.println("Enter the User's email id");
-        String emailID = scan.next();
+        String emailID = scan.nextLine();
+        System.out.println("Enter the User's mobile no");
+        String mobileNo = scan.nextLine();
         System.out.println("Given first name : " + firstName + " is valid : " + validName(firstName));
         System.out.println("Given last name : " + lastName + " is valid : " + validName(lastName));
         System.out.println("Given email id : " + emailID + " is valid : " + validEmail(emailID));
+        System.out.println("Given mobile no : " + mobileNo + " is valid : " + validPhoneNo(mobileNo));
     }
 
     // Method to check username
@@ -38,13 +43,26 @@ public class UserRegistration {
     }
 
     // Method to check email id
-    public static boolean validEmail(String name) {
+    public static boolean validEmail(String id) {
         boolean flag = false;
-        String regex = "^[a-z0-9+_.-]+@+[a-z]+.+[a-z]$";
+        String regex = "^[a-z0-9+_.-]+@+[a-z]+.+[a-z]+.+[a-z]$";
         Pattern pattern = Pattern.compile(regex);
-        for(int i = 0; i < name.length(); i++)
+        for(int i = 0; i < id.length(); i++)
         {
-            Matcher matcher=pattern.matcher(name);
+            Matcher matcher=pattern.matcher(id);
+            flag = matcher.matches();
+        }
+        return flag;
+    }
+
+    // Method to check mobile
+    public static boolean validPhoneNo(String phoneNo) {
+        boolean flag = false;
+        String regex = "^\\++[91]+\\s+[789]\\d{9}$";
+        Pattern pattern = Pattern.compile(regex);
+        for(int i = 0; i < phoneNo.length(); i++)
+        {
+            Matcher matcher=pattern.matcher(phoneNo);
             flag = matcher.matches();
         }
         return flag;
